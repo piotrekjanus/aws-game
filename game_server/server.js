98d6cb606aa392387b95000f1a75daba10a9ca2a
ramusz1 <ramusz@wp.pl>
2019-05-25 16:41:54 +0200

    id generator

--- game_server/server.js
+++ game_server/server.js
@@ -8,8 +8,20 @@ var server = http.listen(port, function(){
 
 var io = require('socket.io')(server);
 
+// define player id generator
+function * generator_func(){
+    var id = 0;
+    while(true){
+        yield id;
+        id++;
+    }
+}
+var id_generator = generator_func()
+
 io.on('connection', function(socket){
   console.log('a user connected');
-});
+  var id = id_generator.next().value
+  
 
+});
 
