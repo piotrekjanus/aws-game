672346c248af6b3d713d25a361de68e727dd5b14
Piotr <piotrek.janus96@gmail.com>
2019-05-31 22:18:14 +0200

    code refactor

--- game/tabs/models.py
+++ game/tabs/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.conf import settings
-# Create your models here.
+
+
 class Result(models.Model):
-    date     = models.DateField()
-    player_1 =  models.ForeignKey(
+    date = models.DateField()
+    player_1 = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
-        related_name='player_1'
-    )
-    player_2 =  models.ForeignKey(
+        related_name='player_1')
+
+    player_2 = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
-        related_name='player_2'
-    )
-    winner =  models.ForeignKey(
+        related_name='player_2')
+
+    winner = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.PROTECT,
-        related_name='winner'
-    )
+        related_name='winner')
--- game/tabs/views.py
+++ game/tabs/views.py
@@ -19,11 +19,12 @@ def game_view(request):
 @login_required(login_url='login')
 def stats_view(request):
     username = None
+    context = {}
     if request.user.is_authenticated:
         username = request.user.username
-    obj = Result.objects.filter(Q(player_1__username=username) | Q(player_2__username=username))
-    context = {"obj": obj,
-               "user": username}
+        obj = Result.objects.filter(Q(player_1__username=username) | Q(player_2__username=username))
+        context = {"obj": obj,
+                   "user": username}
     return render(request, 'tabs/stats.html', context)
 
 @login_required(login_url='login')
@@ -34,15 +35,18 @@ def home_view(request):
 def get_match_result(request):
     if request.POST:
         status = request.POST.getlist('info')[0]
-        users = json.loads( request.POST.getlist('users')[0])
+        users = json.loads(request.POST.getlist('users')[0])
         if request.user.is_authenticated:
             user_name = request.user.username
         result = Result()
-        print(user_name)
         result.date = datetime.datetime.utcnow()
         result.player_1 = User.objects.get(username=users[0])
         result.player_2 = User.objects.get(username=users[1])
         if(status == 'winner'):
             result.winner = User.objects.get(username=user_name)
-            result.save()    
-    return render(request, 'index.html', {})
+            result.save()
+        return render(request, 'index.html', {})
+    elif request.GET:
+        return HttpResponse()
+    else:
+        return HttpResponse()
