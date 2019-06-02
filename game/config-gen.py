403c183b1d0da9c010db9c2ec47b643d93885038
ramusz1 <ramusz@wp.pl>
2019-06-03 01:14:39 +0200

    auto config added

--- game/config-gen.py
+++ game/config-gen.py
@@ -8,9 +8,23 @@ parser.add_argument('--game_host',
                     required = True,
                     help='game host name')
 
+parser.add_argument('--game_port',
+                    metavar='game_port',
+                    type = int,
+                    required = True,
+                    help='game port name')
+
 args = parser.parse_args()
 
-config = { 'game_host' : args.game_host}
+# write config
+config = { 'game_host' : '{}:{}'.format(args.game_host, args.game_port)}
+
 config_content = 'var config = ' + json.dumps(config) + ';'
 with open('static/config/config.js', 'w+') as f:
     f.write(config_content)
+
+
+config = "ALLOWED_HOSTS = ['{}','localhost']".format(args.game_host)
+
+with open('autoconfig.py', 'w+') as f:
+    f.write(config)
