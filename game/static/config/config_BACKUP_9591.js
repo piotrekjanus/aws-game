0b17dfd96a38bfe3d50f710a8130684386345b76
MichalTurski <turski.michal.antoni@gmail.com>
2019-06-04 16:40:53 +0200

    Reconfigured enviroment

---
+++ game/static/config/config_BACKUP_9591.js
@@ -0,0 +1,5 @@
+<<<<<<< HEAD
+var config = {"game_host": "gameserver-env.tdaph9jptk.eu-central-1.elasticbeanstalk.com"};
+=======
+var config = {"game_host": "localhost:6969"};
+>>>>>>> d6297724f0da696ce865c4b3e483725c3a78f032
